f73ee46279dd35110ec96d1942f54ed53908a2cc
Fikri Zaki Aditama, S.Si. <fikriyomann@gmail.com>
2025-02-21 15:15:33 +0700

    update

--- Assets/Core/Game Mode/TutorialMenu.cs
+++ Assets/Core/Game Mode/TutorialMenu.cs
@@ -4,24 +4,43 @@ public class TutorialMenu : MonoBehaviour
 {
     public GameObject panelTutorial;
     public GameObject panelGrammar;
+    public GameObject panelCaraMain;
 
     public void OpenTutorialPanel()
     {
         panelTutorial.SetActive(true);
+        panelCaraMain.SetActive(false);
+        panelGrammar.SetActive(false);
     }
 
     public void CloseTutorialPanel()
     {
+        panelCaraMain.SetActive(false);
+        panelGrammar.SetActive(false);
         panelTutorial.SetActive(false);
     }
 
     public void OpenGrammarPanel()
     {
         panelGrammar.SetActive(true);
+        panelCaraMain.SetActive(false);
     }
 
     public void CloseGrammarPanel()
     {
+        panelCaraMain.SetActive(false);
+        panelGrammar.SetActive(false);
+    }
+
+    public void OpenCaraBermain()
+    {
+        panelCaraMain.SetActive(true);
+        panelGrammar.SetActive(false);
+    }
+
+    public void CloseCaraBermain()
+    {
+        panelCaraMain.SetActive(false);
         panelGrammar.SetActive(false);
     }
 }
